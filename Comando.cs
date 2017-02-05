44cfad47461556703d934b256c6924fb926b9a8d
Ricardo de Magalhães Simões <rmagalhaess@hotmail.com>
2017-02-05 17:58:56 -0200

    Arquivos

---
+++ Comando.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Desligar
+{
+    class Comando
+    {
+        // ===========================================================================
+            Process Processo = null;
+            ProcessStartInfo Parametros = null;
+        
+        // ===========================================================================
+        public Comando()
+        {
+            this.Processo = new Process();
+            this.Parametros = new ProcessStartInfo();
+
+            this.Parametros.WindowStyle = ProcessWindowStyle.Hidden;
+            this.Parametros.FileName = "cmd.exe";
+        }
+
+        // ===========================================================================
+        public bool Executar( string p_Comando )
+        {
+            // Variáveis:
+                bool Executar = false;
+
+            // Programa
+                this.Parametros.Arguments = p_Comando;
+                this.Processo.StartInfo = this.Parametros;
+                Executar = this.Processo.Start();
+            /*
+                MessageBox.Show( p_Comando, 
+                                 "Comando",
+                                 MessageBoxButtons.OKCancel, 
+                                 MessageBoxIcon.Asterisk);
+            */
+
+            // Fim
+                return Executar;
+        }
+
+    }
+}
